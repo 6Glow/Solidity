00742feca40e98ecfbcb0d0014a43fa23c980b52
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-04-16 23:16:10 +0300

    creating logic for Calculator.sol

---
+++ solidity-calculator/Calculator.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.25;
+
+contract Calculator {
+ uint256 public result;
+ 
+ function add(uint256 a, uint256 b) public {
+ result = a + b;
+ }
+ function subtract(uint256 a, uint256 b) public {
+ result = a - b;
+ }
+ 
+ modifier nonZero(uint256 number) {
+ require(number != 0, "Number cannot be zero");
+ _;
+ }
+ 
+ function divide(uint256 a, uint256 b) public nonZero(b) {
+ result = a / b;
+ }
+ // other functions go here
+}
